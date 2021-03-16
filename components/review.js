9409bfe3ae4ab00c79ed9a3c9aa328319203e665
rushadmannan <rushadmannan@gmail.com>
2021-03-16 15:19:36 +0600

    most_prolly_final

--- components/review.js
+++ components/review.js
@@ -3,7 +3,7 @@ var count = 0 ; // Number of likes fetched from database
 if(_like){
     _like.addEventListener('click', e =>{
         if(count % 2 == 0){
-            document.getElementById('like').style.color="blue";
+            document.getElementById('like').style.color="#4267B2";
             count++;
             console.log(count);
         }else{
